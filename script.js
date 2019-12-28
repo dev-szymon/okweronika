7551b279fed46fe92f0fa588a7871b9337cc9894
Szymon <szymonphotoshop@gmail.com>
2019-12-28 19:51:58 +0100

    final

--- script.js
+++ script.js
@@ -25,41 +25,51 @@ cmngSoon.addEventListener("click", showSoon);
 function showSoon() {
   window.scrollTo(0, 0);
   mainNav.classList.toggle("hidden-nav");
+  backgroundTop.classList.remove("opened-about");
+  backgroundTop.classList.add("opened-soon");
   header.classList.add("active-section");
   sectionsAll.forEach(e => e.classList.add("hidden"));
   navMobDesk.forEach(e => e.classList.add("hidden"));
   soonTab.classList.remove("hidden");
+  isSectionOpen = true;
 }
 
 aboutMe.addEventListener("click", hideMainNav);
 
 function hideMainNav() {
+  backgroundTop.classList.remove("opened-soon");
+  backgroundTop.classList.add("opened-about");
   window.scrollTo(0, 0);
   mainNav.classList.toggle("hidden-nav");
-  backgroundTop.classList.add("opened-about");
   header.classList.add("active-section");
   sectionsAll.forEach(e => e.classList.add("hidden"));
   bioSection.classList.remove("hidden");
   navMobDesk.forEach(e => e.classList.remove("hidden"));
   btns.forEach(e => e.classList.remove("active-tab"));
   bioBtn.forEach(e => e.classList.add("active-tab"));
+  isSectionOpen = true;
 }
 
 homeBtn.forEach(e => e.addEventListener("click", goHome));
 
 function goHome() {
-  mainNav.classList.toggle("hidden-nav");
+  if (isSectionOpen === true) {
+    mainNav.classList.toggle("hidden-nav");
+  }
   window.scrollTo(0, 0);
   backgroundTop.classList.remove("opened-about");
+  backgroundTop.classList.remove("opened-soon");
   header.classList.remove("active-section");
   sectionsAll.forEach(e => e.classList.add("hidden"));
   navMobDesk.forEach(e => e.classList.add("hidden"));
+  isSectionOpen = false;
 }
 
 function resetTabs() {
   sectionsAll.forEach(e => e.classList.add("hidden"));
   window.scrollTo(0, 0);
   btns.forEach(e => e.classList.remove("active-tab"));
+  isSectionOpen = true;
 }
 
 bioBtn.forEach(e => e.addEventListener("click", openBio));
@@ -105,6 +115,16 @@ const slvkBtn = document.querySelector("#slvk-btn");
 const espBtn = document.querySelector("#esp-btn");
 const usaBtn = document.querySelector("#usa-btn");
 const gbBtn = document.querySelector("#gb-btn");
+const slvkSmBtn = document.querySelectorAll(".slvk-btn");
+const espSmBtn = document.querySelectorAll(".esp-btn");
+const usaSmBtn = document.querySelectorAll(".usa-btn");
+const gbSmBtn = document.querySelectorAll(".gb-btn");
+const flagSmIcons = document.querySelectorAll(".flag-icon");
+
+slvkSmBtn.forEach(e => e.addEventListener("click", showSlvkTab));
+espSmBtn.forEach(e => e.addEventListener("click", showEspTab));
+usaSmBtn.forEach(e => e.addEventListener("click", showUsaTab));
+gbSmBtn.forEach(e => e.addEventListener("click", showGbTab));
 
 slvkBtn.addEventListener("click", showSlvkTab);
 espBtn.addEventListener("click", showEspTab);
@@ -114,19 +134,27 @@ gbBtn.addEventListener("click", showGbTab);
 function showSlvkTab() {
   resetTabs();
   slvkTab.classList.remove("hidden");
+  flagSmIcons.forEach(e => e.classList.remove("active-flag"));
+  slvkSmBtn.forEach(e => e.classList.add("active-flag"));
 }
 
 function showEspTab() {
   resetTabs();
   espTab.classList.remove("hidden");
+  flagSmIcons.forEach(e => e.classList.remove("active-flag"));
+  espSmBtn.forEach(e => e.classList.add("active-flag"));
 }
 
 function showUsaTab() {
   resetTabs();
   usaTab.classList.remove("hidden");
+  flagSmIcons.forEach(e => e.classList.remove("active-flag"));
+  usaSmBtn.forEach(e => e.classList.add("active-flag"));
 }
 
 function showGbTab() {
   resetTabs();
   gbTab.classList.remove("hidden");
+  flagSmIcons.forEach(e => e.classList.remove("active-flag"));
+  gbSmBtn.forEach(e => e.classList.add("active-flag"));
 }
